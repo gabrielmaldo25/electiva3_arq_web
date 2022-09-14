35e27758fc8f41e2add08ed1bba38fbadc19c26e
Gabriel <maldoangel@hotmail.com>
2022-09-14 14:36:41 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Layout from "../src/components/layout";
+function App() {
+  return (
+    <Layout>
+      <div className="text-white">Contenido</div>
+    </Layout>
+  );
+}
+
+export default App;
