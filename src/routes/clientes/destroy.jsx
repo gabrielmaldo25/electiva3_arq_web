623d7572a0f2a0f08cabe7341e0a89f41463c427
Alexis Ortiz <olmedo.aortiz@gmail.com>
2022-11-02 22:18:37 -0300

    [INIT] Init Java project

--- src/routes/clientes/destroy.jsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "react-router-dom";
-import { deleteCliente } from "./clientes";
-
-export async function action({ params }) {
-  await deleteCliente(params.clienteId);
-  return redirect("/clientes");
-}
---
+++ src/test/java/com/proyecto/electiva3/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.electiva3.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
