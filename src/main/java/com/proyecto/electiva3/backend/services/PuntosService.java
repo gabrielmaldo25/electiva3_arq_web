72cd1ce24f17a05c7a40c769bcb9c6dc9c334bbf
Alexis Ortiz <olmedo.aortiz@gmail.com>
2022-11-07 20:24:23 -0300

    [MOD] Error al usar los puntos.

--- src/main/java/com/proyecto/electiva3/backend/services/PuntosService.java
+++ src/main/java/com/proyecto/electiva3/backend/services/PuntosService.java
@@ -2,6 +2,8 @@ package com.proyecto.electiva3.backend.services;
 
 import com.proyecto.electiva3.backend.model.*;
 import com.proyecto.electiva3.backend.repository.BolsaPuntosRepository;
+import com.proyecto.electiva3.backend.repository.PuntosCabeceraRepository;
+import com.proyecto.electiva3.backend.repository.PuntosDetalleRepostitory;
 import com.proyecto.electiva3.backend.repository.ReglasPuntosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +29,12 @@ public class PuntosService {
 
     @Autowired
     private ParamPuntosService paramPuntosService;
+
+    @Autowired
+    private PuntosCabeceraRepository puntosCabeceraRepository;
+
+    @Autowired
+    private PuntosDetalleRepostitory puntosDetalleRepostitory;
 
     /* Registrar puntos a un cliente */
     public void registrarPuntos(Long idCliente, Float monto) throws Exception {
@@ -70,7 +78,7 @@ public class PuntosService {
     }
 
     /* Usar puntos disponibles */
-    public void usarPuntos(Long idCliente, Long idConcepto) throws Exception {
+    public void usarPuntos(Long idCliente, Long idConcepto) throws Exception    {
         Cliente cliente = clienteService.findById(idCliente);
         Concepto concepto = conceptoService.findById(idConcepto);
         List<BolsaPuntos> bolsaPuntos = new ArrayList<>();
@@ -94,22 +102,43 @@ public class PuntosService {
             throw new Exception("El cliente no cuenta con suficientes puntos.");
         }
 
-        // si existe suficientes puntos, descontar de las bolsas
+        // si existe suficientes puntos, descontar de las bolsas y registrar
+        // en puntos_cabecera y puntos_detalle
         puntosConcepto = concepto.getPuntos();
+
+        PuntosCabecera puntosCab = new PuntosCabecera();
+        puntosCab.setCliente(cliente);
+        puntosCab.setConcepto(concepto);
+        puntosCab.setFecha(LocalDate.now());
+        puntosCab.setPuntajeUtilizado(puntosConcepto);
+        puntosCabeceraRepository.save(puntosCab);
+
         for(BolsaPuntos bolsa : bolsaPuntos) {
-            if(bolsa.getPuntosSaldo() > puntosConcepto) {
+            if(bolsa.getPuntosSaldo() == 0) continue;
+
+            PuntosDetalle puntosDet = new PuntosDetalle();
+            puntosDet.setBolsaPuntos(bolsa);
+            puntosDet.setPuntosCabecera(puntosCab);
+
+            if(bolsa.getPuntosSaldo() >= puntosConcepto) {
                 bolsa.setPuntosUsados(bolsa.getPuntosUsados() + puntosConcepto);
                 bolsa.setPuntosSaldo(bolsa.getPuntosSaldo() - puntosConcepto);
+                puntosDet.setPuntajeUtilizado(puntosConcepto); // registrar puntos utilizados
                 bolsaPuntosRepository.save(bolsa);
+                puntosCab.getPuntosDetalles().add(puntosDet);
+                puntosDetalleRepostitory.save(puntosDet);
                 break;
             } else {
                 puntosConcepto -= bolsa.getPuntosSaldo();
+                puntosDet.setPuntajeUtilizado(bolsa.getPuntosSaldo()); // registrar puntos utilizados
                 bolsa.setPuntosUsados(bolsa.getPuntosUsados() + bolsa.getPuntosSaldo());
                 bolsa.setPuntosSaldo(0f);
-                bolsaPuntosRepository.delete(bolsa);
+                bolsaPuntosRepository.save(bolsa);
             }
+            puntosCab.getPuntosDetalles().add(puntosDet);
+            puntosDetalleRepostitory.save(puntosDet);
         }
-
+        puntosCabeceraRepository.save(puntosCab);
         System.out.println("Puntos utilizados exitosamente");
 
         return;
