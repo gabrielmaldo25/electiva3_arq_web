82142b9271f9b3d451359228b972baec8e5cec09
Alexis Ortiz <olmedo.aortiz@gmail.com>
2022-11-28 18:21:43 -0300

    [ADD] Login page.

--- src/main/java/com/proyecto/electiva3/backend/services/UsuarioService.java
+++ src/main/java/com/proyecto/electiva3/backend/services/UsuarioService.java
@@ -1,11 +1,13 @@
 package com.proyecto.electiva3.backend.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -13,9 +15,12 @@ import java.util.*;
 @Service
 public class UsuarioService implements UserDetailsService {
 
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if(!username.equals("admin")) throw new UsernameNotFoundException("Username incorrecto");
-        return new User("admin", "12345", mapearAutoridadesRoles("conectarse"));
+        return new User("admin", passwordEncoder.encode("12345"), mapearAutoridadesRoles("conectarse"));
     }
     private Collection<? extends GrantedAuthority> mapearAutoridadesRoles(String permiso) {
         Set<SimpleGrantedAuthority> permisos = new HashSet<>();
